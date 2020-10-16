5dd2793ad694fe383506571532e803355f1a7d51
Sivashankar <69213097+sivashankar1810@users.noreply.github.com>
2020-10-16 14:46:33 +0530

    Add files via upload

---
+++ build/precache-manifest.a4e3cbbd214d4d06b26f1c4566851720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45aead92259fadd2d357d0832c025636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42789501d5d5a96798d5",
+    "url": "/static/css/2.a444aa93.chunk.css"
+  },
+  {
+    "revision": "96bf643c34bd1eb6c5ce",
+    "url": "/static/css/main.414c0224.chunk.css"
+  },
+  {
+    "revision": "42789501d5d5a96798d5",
+    "url": "/static/js/2.09413417.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.09413417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bf643c34bd1eb6c5ce",
+    "url": "/static/js/main.606b4149.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
